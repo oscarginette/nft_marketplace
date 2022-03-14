1e2f09514270e5eb2327d7997a2b12199445cc93
oscar.ginette <oscar.ginette@dynatrace.com>
2022-03-14 18:43:53 +0100

    Create makeItem function

--- src/backend/contracts/Marketplace.sol
+++ src/backend/contracts/Marketplace.sol
@@ -20,9 +20,29 @@ contract Marketplace is ReentrancyGuard {
         bool sold;
     }
 
+    // itemId -> Item
+    mapping(uint => Item) public items;
+
     constructor(uint _feePercent) {
         feeAccount = payable(msg.sender);
         feePercent = _feePercent;
+    }
+
+    // In the front end, you just need to send the contract address and solidity will
+    // automatically convert this to a IERC721 contract
+
+    // nonReentrant modifier: prevents bad guys to call this function before it has finished the first time
+    function makeItem(IERC721 _nft, uint _tokenId, uint _price) external nonReentrant {
+        require(_price > 0, "Price must be greater than zero");
+        _nft.transferFrom(msg.sender, address(this), _tokenId);
+        items[itemsCount] = Item (
+            itemCount,
+            _nft,
+            _tokenId,
+            _price,
+            payable(msg.sender),
+            false
+        );
     }
 
 }
