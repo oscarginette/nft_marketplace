6d6a85cdc79857106fa2eb3fedb4b8e5df78d2c6
oscar.ginette <oscar.ginette@dynatrace.com>
2022-03-24 17:25:25 +0100

    Create main functions: login and load contracts

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,8 +1,40 @@
 
 import logo from './logo.png';
 import './App.css';
+import { useState } from 'react';
+import { ethers } from "ethers";
+import MarketplaceAbi from '../contractsData/Marketplace.json'
+import MarketplaceAddress from '../contractsData/Marketplace-address.json'
+import NFTAbi from '../contractsData/NFT.json'
+import NFTAddress from '../contractsData/NFT-address.json'
  
 function App() {
+  const [loading, setLoading] = useState(TextTrackCue);
+  const [account, setAccount] = useState(null);
+  const [nft, setNFT] = useState({});
+  const [marketplace, setMarketplace] = useState({});
+
+  const web3Handler = async () => {
+    // Get provider from Metamask
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts'});
+    setAccount(accounts);
+    // Get provider from Metamask
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // Set signer
+    const signer = provider.getSigner();
+
+    loadContracts(signer);
+  }
+
+  const loadContracts = async (signer) => {
+    // Get deployed copies of contracts
+    const marketplace = new ethers.Contract(MarketplaceAddress.address, MarketplaceAbi.abi, signer);
+    setMarketplace(marketplace);
+    const nft = new ethers.Contract(NFTAddress.address, NFTAbi.abi, signer);
+    setNFT(nft);
+    setLoading(false);
+  }
+
   return (
     <div>
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
