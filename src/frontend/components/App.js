705079b87a52453600efd0bb0237d9b383266ea7
oscar.ginette <oscar.ginette@dynatrace.com>
2022-03-28 17:09:43 +0200

    Create new components

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,22 +1,28 @@
-
-import logo from './logo.png';
-import './App.css';
-import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { useState } from "react";
 import { ethers } from "ethers";
-import MarketplaceAbi from '../contractsData/Marketplace.json'
-import MarketplaceAddress from '../contractsData/Marketplace-address.json'
-import NFTAbi from '../contractsData/NFT.json'
-import NFTAddress from '../contractsData/NFT-address.json'
- 
+import MarketplaceAbi from "../contractsData/Marketplace.json";
+import MarketplaceAddress from "../contractsData/Marketplace-address.json";
+import NFTAbi from "../contractsData/NFT.json";
+import NFTAddress from "../contractsData/NFT-address.json";
+import Navigation from "./Navbar";
+import Create from "./Create";
+import Home from "./Home";
+import MyListedItems from "./MyListedItems";
+import MyPurchases from "./MyPurchases";
+import { Spinner } from "react-bootstrap";
+
 function App() {
-  const [loading, setLoading] = useState(TextTrackCue);
+  const [loading, setLoading] = useState(true);
   const [account, setAccount] = useState(null);
   const [nft, setNFT] = useState({});
   const [marketplace, setMarketplace] = useState({});
 
   const web3Handler = async () => {
-    // Get provider from Metamask
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts'});
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
     setAccount(accounts);
     // Get provider from Metamask
     const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -24,58 +30,42 @@ function App() {
     const signer = provider.getSigner();
 
     loadContracts(signer);
-  }
+  };
 
   const loadContracts = async (signer) => {
     // Get deployed copies of contracts
-    const marketplace = new ethers.Contract(MarketplaceAddress.address, MarketplaceAbi.abi, signer);
+    const marketplace = new ethers.Contract(
+      MarketplaceAddress.address,
+      MarketplaceAbi.abi,
+      signer
+    );
     setMarketplace(marketplace);
     const nft = new ethers.Contract(NFTAddress.address, NFTAbi.abi, signer);
     setNFT(nft);
     setLoading(false);
-  }
+  };
 
   return (
-    <div>
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a
-          className="navbar-brand col-sm-3 col-md-2 ms-3"
-          href="http://www.dappuniversity.com/bootcamp"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Dapp University
-        </a>
-      </nav>
-      <div className="container-fluid mt-5">
-        <div className="row">
-          <main role="main" className="col-lg-12 d-flex text-center">
-            <div className="content mx-auto mt-5">
-              <a
-                href="http://www.dappuniversity.com/bootcamp"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={logo} className="App-logo" alt="logo"/>
-              </a>
-              <h1 className= "mt-5">Dapp University Starter Kit</h1>
-              <p>
-                Edit <code>src/frontend/components/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="http://www.dappuniversity.com/bootcamp"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-              </a>
-            </div>
-          </main>
-        </div>
+    <BrowserRouter>
+      <div>
+        <Navigation web3Handler={web3Handler} account={account} />
+        { loading ? (
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '80vh'}}>
+            <Spinner animation="border" style={{ display: 'flex' }}/>
+            <p className="mx-3 my-0"> Awaiting Metamask Connection...</p>
+          </div>
+        ) : (
+          <Routes>
+            <Route path="/" element={
+              <Home/>
+            } />
+            <Route path="/create" />
+            <Route path="/my-listed-items" />
+            <Route path="/my-purchases" />
+          </Routes>
+        )}
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
